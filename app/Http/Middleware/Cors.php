1b740fb1cf12dc22304deeeb38f1702ee5ca02e6
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-11-03 09:24:24 -0500

    changed cors config

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
         $response = $next($request);
 
         $response
-            ->header('Access-Control-Allow-Origin', env('CLIENT_URL'))
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
         return $response;
